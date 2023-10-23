72c1f8235bb60951e3da5cab4985cfb66450b209
Kari Laari <kari@karin-atk.fi>
2023-10-23 21:40:16 +0300

    WIP: bar visualizer

---
+++ index.js
@@ -0,0 +1,35 @@
+let audio1 = new Audio();
+audio1.src = "./audio1.mp3";
+
+const audioCtx = new AudioContext();
+console.log(audioCtx);
+
+const container = document.getElementById("container");
+const canvas = document.getElementById("canvas1");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext("2d");
+let audioSource;
+let analyser;
+
+container.addEventListener("click", function () {
+    audio1.play();
+    audioSource = audioCtx.createMediaElementSource(audio1);
+    analyser = audioCtx.createAnalyser();
+    audioSource.connect(analyser);
+    analyser.connect(audioCtx.destination);
+    analyser.fftSize = 64;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    const barWidth = canvas.width / bufferLength;
+    let barHeight;
+    let x = 0;
+
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+});
+
+// https://youtu.be/VXWvfrmpapI?si=ohIiSZ7SAiTmxuS7&t=1778
